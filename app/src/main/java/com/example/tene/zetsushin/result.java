4d61a30de32296ce40699b759ae37ef3bcf2e5b1
yu-enpit27 <u004ff@gmail.com>
2017-11-10 11:46:46 +0900

    c

--- app/src/main/java/com/example/tene/zetsushin/result.java
+++ app/src/main/java/com/example/tene/zetsushin/result.java
@@ -1,31 +1,33 @@
 package com.example.tene.zetsushin;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class result extends AppCompatActivity {
 
-    int sum = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int sum = 0;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        // ラジオグループのオブジェクトを取得
-        RadioGroup group1 = (RadioGroup)findViewById(R.id.q1_rg);
-        // チェックされているラジオボタンの ID を取得
-        int id = group1.getCheckedRadioButtonId();
-/*
-        TextView textView = (TextView) findViewById(R.id.score);
-        if(id == 1){
-            sum = 100;
-            textView.setText(id);
-        } else {
-            sum = 50;
-            textView.setText("50");
-        }*/
+        Intent intent = getIntent();
+
+        int q1_a = intent.getIntExtra("q1_answer",0);
+        int q2_a = intent.getIntExtra("q2_answer",0);
+
+        if(q1_a==2131558543) sum=sum+50;
+        if(q2_a==2131558543) sum=sum+50;
+
+        TextView ScoreView = (TextView) findViewById(R.id.score);
+        ScoreView.setText(String.valueOf(sum));
+
+        //TextView IdView = (TextView) findViewById(R.id.IdView);
+        //IdView.setText(String.valueOf(q2_a));
 
     }
 
--- app/src/main/java/com/example/tene/zetsushin/test.java
+++ app/src/main/java/com/example/tene/zetsushin/test.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.RadioGroup;
 
 public class test extends AppCompatActivity {
 
@@ -11,6 +12,19 @@ public class test extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        /*
+
+        追加
+
+         */
+
+
+        /*
+
+        ここまで
+
+        */
+
     }
 
     /*
@@ -19,7 +33,16 @@ public class test extends AppCompatActivity {
 
      */
     public void onNextResultTapped(View view) {
+
+        RadioGroup q1_radioGroup = (RadioGroup) findViewById(R.id.q1_rg);
+        int q1_checkedId = q1_radioGroup.getCheckedRadioButtonId();
+        RadioGroup q2_radioGroup = (RadioGroup) findViewById(R.id.q2_rg);
+        int q2_checkedId = q2_radioGroup.getCheckedRadioButtonId();
+
+
         Intent intent = new Intent(this, result.class);
+        intent.putExtra("q1_answer",q1_checkedId);
+        intent.putExtra("q2_answer",q2_checkedId);
         startActivity(intent);
     }
     /*
