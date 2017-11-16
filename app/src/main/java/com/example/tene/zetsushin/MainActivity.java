46fd6d6b2c6d4e3f7152cf5ee821222a7b8536c0
u004ff <u004ff@gmail.com>
2017-11-16 11:03:59 +0900

    c

--- app/src/main/java/com/example/tene/zetsushin/MainActivity.java
+++ app/src/main/java/com/example/tene/zetsushin/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        String out_id = intent.getStringExtra("InputID");
+        TextView IdView = (TextView) findViewById(R.id.output_id);
+        IdView.setText(out_id);
     }
 
     /*
